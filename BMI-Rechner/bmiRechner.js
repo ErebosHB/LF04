05072fb47b7a1f31d555a67a1a65a13fe869c99f
Erebos.hb <yannick.s3@schule.bremen.de>
2022-11-21 13:15:42 +0100

    bmi

--- BMI-Rechner/bmiRechner.js
+++ BMI-Rechner/bmiRechner.js
@@ -1,8 +1,8 @@
 let BmiRechner = () => {
     let groesse = document.getElementById("groesse").value;
     let gewicht = document.getElementById("gewicht").value;
-    let bmi = gewicht/Math.pow(groesse/100, 2);
-    document.getElementById("bmiOutput").value = bmi;
+    let bmi =gewicht/Math.pow(groesse/100, 2);
+    document.getElementById("bmiOutput").value = bmi.toFixed(2);
 
 }
 document.getElementById("taste").addEventListener('click', BmiRechner);
